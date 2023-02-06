98ab27d00bd9c59266abb1db8414771d7267d36a
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-06 22:29:09 +0500

    Add models and associations

---
+++ app/models/recipe.rb
@@ -0,0 +1,6 @@
+class Recipe < ApplicationRecord
+
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id'
+
+end

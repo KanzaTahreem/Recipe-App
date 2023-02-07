54728d52a964dfe9bec3b4a2b57d3d20ac266010
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-07 21:10:29 +0500

    Add an index action to get all recipes of user

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,14 @@
 class RecipesController < ApplicationController
-  def index; end
+  before_action :find_user, expect: [:update]
+
+  
+  def index
+    @recipes = @user.recipes.all
+  end
+
+  private
+
+  def find_user
+    @user = current_user
+  end
 end

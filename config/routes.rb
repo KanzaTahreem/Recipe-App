2b898d9f35dbf8185820d65de87aa18fde1b7bd9
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-07 21:22:18 +0500

    Add routes corresponding recipe controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :recipes, only: [:index] do
+  resources :recipes, except: [:update] do
   end
 
   # Defines the root path route ("/")

5d798e99fae57029cc9db4d412840bdaf3362249
KanzaTahreem <tehreemmsk411@gmail.com>
2023-02-09 20:08:29 +0500

    Add index action and view page for public recipes

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,2 +1,6 @@
 class PublicRecipesController < ApplicationController
+  def index
+    @users = User.all
+    @public_recipes = Recipe.includes(:recipe_foods).where(public: true).order(created_at: :desc)
+  end
 end

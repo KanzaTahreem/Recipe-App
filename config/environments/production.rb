21cb342444e1ce9ca6863369b08fff09fe4d5f65
KanzaTahreem <tehreemmsk411@gmail.com>
2023-06-20 03:48:35 +0500

    Add mailer environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,20 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Recipe_App_production"
 
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.perform_caching = false
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              ENV['EMAIL_ADDRESS'],
+    port:                 587,
+    domain:               ENV['DOMAIN'],
+    user_name:            ENV['EMAIL_USER_NAME'],
+    password:             ENV['EMAIL_PASS'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
